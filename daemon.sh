b6423ef8e8e4a8108466ce6a9757b37e34ff4183
Zyrsa <zyrsa@zyrsa.org>
2018-07-28 17:43:57 +0300

    updated to run.pid

--- daemon.sh
+++ daemon.sh
@@ -2,8 +2,8 @@
 
 cd ~/twircbot
 
-if [ -f ~/twircbot/twircbot.pid ]; then
-	ps up `cat ~/twircbot/twircbot.pid ` >/dev/null || /usr/bin/python3.5 ~/twircbot/twircbot.py >/dev/null
+if [ -f ~/twircbot/run.pid ]; then
+	ps up `cat ~/twircbot/run.pid ` >/dev/null || /usr/bin/python3.5 ~/twircbot/twircbot.py >/dev/null
 else
 	/usr/bin/python3.5 ~/twircbot/twircbot.py >/dev/null
 fi
